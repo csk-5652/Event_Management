6f1c6020785c35fb5f77ae4f62190d8bd72e6e06
csk-5652 <79008598+csk-5652@users.noreply.github.com>
2021-12-10 09:45:47 +0530

    Update settings.py

--- Event_Management/settings.py
+++ Event_Management/settings.py
@@ -143,11 +143,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #smtp configuration
 
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'chinmaykadam036@protonmail.com'
-# EMAIL_HOST_PASSWORD = 'ChinmaY@036'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'chinmaykadam036@protonmail.com'
+EMAIL_HOST_PASSWORD = 'ChinmaY@036'
 
 
