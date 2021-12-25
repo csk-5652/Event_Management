f1e5cfd1b8ef5c57dd723bac724fd77d5f6b57c3
csk-5652 <79008598+csk-5652@users.noreply.github.com>
2021-12-25 11:56:11 +0530

    Update settings.py

--- Event_Management/settings.py
+++ Event_Management/settings.py
@@ -147,7 +147,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_USE_TLS = False
-EMAIL_HOST_USER = 'chinmaykadam.stud@vivacollege.org'
-EMAIL_HOST_PASSWORD = 'chinmay_viva@123'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
